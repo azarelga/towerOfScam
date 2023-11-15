f33f36f82223d5cf0da740b7c2f8e89e1d122d8f
Azarel Grahandito <skootydack@gmail.com>
2023-11-15 10:41:55 +0700

    feat: new methods for Judol class

--- src/com/towerofscam/characters/Judol.java
+++ src/com/towerofscam/characters/Judol.java
@@ -1,10 +1,68 @@
 package com.towerofscam.characters;
 
+import com.towerofscam.atribut.Item;
+
 public class Judol {
 	private int energy;
 	private int postXJ;
 	private int postYJ;
 	private int scoreMax;
 	private int currentLevel;
-	
+
+	public Judol(int currentLevel) {
+		this.energy = 0;
+		this.postXJ = 0;
+		this.postYJ = 0;
+		this.scoreMax = 0;
+		this.currentLevel = currentLevel;
+	}
+
+	public void CastItem(Item item) {
+		if (item.getOperasi() == '+'){
+			this.energy += item.getNumber();
+		} else if (item.getOperasi() == '-') {
+			this.energy -= item.getNumber();
+		} else if (item.getOperasi() == '*') {
+			this.energy *= item.getNumber();
+		} else if (item.getOperasi() == '/') {
+			this.energy /= item.getNumber();
+		}
+	}
+
+	public int GetEnergy() {
+		return this.energy;
+	}
+
+	public void ReceiveEnergy(int energy) {
+		this.energy += energy;
+	}
+
+	public void MoveJudol(int newX, int newY) {
+		this.postXJ = newX;
+		this.postYJ = newY;
+	}
+
+	public int getPostXJ() {
+		return this.postXJ;
+	}
+
+	public int getPostYJ() {
+		return this.postYJ;
+	}
+
+	public int getScoreMax() {
+		return this.scoreMax;
+	}
+
+	public void setScoreMax(int scoreMax) {
+		this.scoreMax = scoreMax;
+	}
+
+	public int getCurrentLevel() {
+		return this.currentLevel;
+	}
+
+	public void setCurrentLevel(int currentLevel) {
+		this.currentLevel = currentLevel;
+	}
 }
